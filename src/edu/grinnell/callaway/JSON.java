108c7aa8ab38fb7cf5673b94096c1c79d3a3de6b
mulhalle <mulhalle@grinnell.edu>
2014-04-15 15:49:46 -0500

    revert JSON class to initial version

--- src/edu/grinnell/callaway/JSON.java
+++ src/edu/grinnell/callaway/JSON.java
@@ -1,12 +1,28 @@
 package edu.grinnell.callaway;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Vector;
 
 public class JSON
 {
+  public Object parse(String str)
+  {
+    switch (str.charAt(0))
+      {
+        case '{':
+          HashMap<String, Object> object =
+              (HashMap<String, Object>) parse(str.substring(1)); // ??
+          break;
+        // return object;
+        case '}':
+          return object; // This should end the object
+        case '[':
+          Vector<Object> array = new Vector<Object>();
+          array = (Vector<Object>) parse(str.substring(1));
+          break;
+        case ']':
 
+      }
+  }
 
 }
