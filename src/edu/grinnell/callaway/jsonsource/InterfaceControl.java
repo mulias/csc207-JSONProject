167b9ff5ba90fee775c2687c9bd3233422796139
emcallaway <callaway@grinnell.edu>
2014-04-25 17:55:42 -0500

    DOCUMENTING ON INTERFACE!!!

--- src/edu/grinnell/callaway/jsonsource/InterfaceControl.java
+++ src/edu/grinnell/callaway/jsonsource/InterfaceControl.java
@@ -1,47 +1,66 @@
 package edu.grinnell.callaway.jsonsource;
 
+import java.io.PrintWriter;
+
 import edu.grinnell.callaway.JSONParser;
 
-/*
- * Citation: http://www.mkyong.com/java/how-to-get-url-content-in-java/
- * http://www.tutorialspoint.com/design_pattern/mvc_pattern.htm
+//Citation: http://www.mkyong.com/java/how-to-get-url-content-in-java/
+//http://www.tutorialspoint.com/design_pattern/mvc_pattern.htm
+/**
+ * Changes the user input obtained by an InterfaceView into a JSON object 
+ * and displays the object as a string.
  */
 public class InterfaceControl
 {
-
+  /**
+   * The InterfaceView used to obtain a JSON string or location.
+   */
   InterfaceView view;
 
+  /**
+   * A constructor for InterfaceControl.
+   * @param view
+   *          an InterfaceView
+   */
   public InterfaceControl(InterfaceView view)
   {
     this.view = view;
   }//SourceControl(SourceModel model, SourceView view)
 
+  /**
+   * Changes the given JSON string into a Java Object representing a 
+   * JSON Object and displays the result as a string.
+   * @return
+   * @throws Exception
+   *            if the input is not a JSON string or the location of one
+   */
   public Object control()
     throws Exception
   {
     JSONParser parser = new JSONParser();
-    
+    PrintWriter pen = new PrintWriter(System.out, true);
+
     Object result = null;
-    String read = null;
-    String string = null;
-    
-    InterfaceView inputPackage = new InterfaceView(read, string);
+
+    InterfaceView inputPackage = new InterfaceView();
     inputPackage.getInputType();
     String inputSignal = inputPackage.parseSignal;
     String packageString = inputPackage.inputString;
-    
-    if (inputSignal.equals("A"))
-      {
-        result = parser.parseFromSource(packageString);////test this guy
-      }
-    else if (inputSignal.equals("B"))
+
+    if (inputSignal.equalsIgnoreCase("A"))
       {
         result = parser.parseFromSource(packageString);
-      }
-    else
+        pen.println("The JSON you entered was: ");
+      } // if (A)
+    else if (inputSignal.equalsIgnoreCase("B"))
       {
-        throw new Exception("wrong input");
-      }
+        result = parser.parseFromSource(packageString);
+        pen.println("The JSON in the file was: ");
+      } // else if (B)
+    else
+      throw new Exception("wrong input");
+
+    pen.println(parser.toJSONString(result));
     return result;
   }//control() 
 }//SourceControl
--- src/edu/grinnell/callaway/jsonsource/InterfaceExp.java
+++ src/edu/grinnell/callaway/jsonsource/InterfaceExp.java
@@ -1,15 +1,22 @@
 package edu.grinnell.callaway.jsonsource;
 
+/**
+ * A simple experiment to show that the interface 
+ * for JSON is working properly.
+ */
 public class InterfaceExp
 {
   /**
+   * Creates an InterfaceView and InterfaceControl to 
+   * display a user interface.
    * @param args
    * @throws Exception 
    */
-  public static void main(String[] args) throws Exception
+  public static void main(String[] args)
+    throws Exception
   {
-    InterfaceView view = new InterfaceView(null, null);
+    InterfaceView view = new InterfaceView();
     InterfaceControl interact = new InterfaceControl(view);
-      interact.control();
-  }
-}
+    interact.control();
+  } // main
+} // class InterfaceExpt
