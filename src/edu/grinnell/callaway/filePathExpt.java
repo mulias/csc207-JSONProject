86c06065d5cf2028f5f21f6d742e1cc1fe9973fa
mulhalle <mulhalle@grinnell.edu>
2014-04-25 18:12:10 -0500

    don't know what I changed, but whatever

--- src/edu/grinnell/callaway/filePathExpt.java
+++ src/edu/grinnell/callaway/filePathExpt.java
@@ -9,10 +9,13 @@ public class filePathExpt
 {
   // http://stackoverflow.com/questions/4871051/getting-the-current-working-directory-in-java
   // http://stackoverflow.com/questions/14169661/read-complete-file-without-using-loop-in-java
-  public static void main(String[] args) throws IOException
+  public static void main(String[] args)
+    throws IOException
   {
     System.out.println("Working Directory = " + System.getProperty("user.dir"));
-    String text = new String(Files.readAllBytes(Paths.get("JSONfile.json")), StandardCharsets.UTF_8);
+    String text =
+        new String(Files.readAllBytes(Paths.get("JSONfile.json")),
+                   StandardCharsets.UTF_8);
     System.out.println(text);
   }
 
