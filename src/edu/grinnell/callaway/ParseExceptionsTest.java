94f484399e5d7e0255fc33098dcf9cf447ddd9bd
mulhalle <mulhalle@grinnell.edu>
2014-04-25 18:11:03 -0500

    add citation

--- src/edu/grinnell/callaway/ParseExceptionsTest.java
+++ src/edu/grinnell/callaway/ParseExceptionsTest.java
@@ -9,6 +9,7 @@ public class ParseExceptionsTest
 
   JSONParser parser = new JSONParser();
 
+  // testing exceptions with junit: http://alexruiz.developerblogs.com/?p=1530
   @Rule
   public ExpectedException thrown = ExpectedException.none();
 
@@ -18,7 +19,7 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON ERROR: no json values found in input");
-    parser.parseFromSource("");
+    parser.parseFromString("");
   }
 
   @Test
@@ -27,7 +28,7 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON ERROR: no json values found in input");
-    parser.parseFromSource(" \t \n \r \b \f ");
+    parser.parseFromString(" \t \n \r \b \f ");
   }
 
   @Test
@@ -36,7 +37,7 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON ERROR(L1:C0): Invalid input character a\na\n^");
-    parser.parseFromSource("a");
+    parser.parseFromString("a");
   }
 
   @Test
@@ -45,7 +46,7 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON ERROR(L1:C0): Invalid input character @\n@\n^");
-    parser.parseFromSource("@");
+    parser.parseFromString("@");
   }
 
   @Test
@@ -54,7 +55,7 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON ERROR(L1:C0): Invalid input character }\n}\n^");
-    parser.parseFromSource("}");
+    parser.parseFromString("}");
   }
 
   @Test
@@ -63,7 +64,7 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON NUMBER ERROR(L1:C0): invalid number 15-\n15-\n^");
-    parser.parseFromSource("15-");
+    parser.parseFromString("15-");
   }
 
   @Test
@@ -72,16 +73,16 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON NUMBER ERROR(L1:C0): invalid number 13+\n13+\n^");
-    parser.parseFromSource("13+");
+    parser.parseFromString("13+");
   }
-  
+
   @Test
   public void numberTest3()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON NUMBER ERROR(L1:C0): invalid number 13E.5\n13E.5\n^");
-    parser.parseFromSource("13E.5");
+    parser.parseFromString("13E.5");
   }
 
   @Test
@@ -90,7 +91,7 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON BOOLEAN ERROR(L1:C3): expected input \"true\"\ntruqqqqqq\n   ^");
-    parser.parseFromSource("truqqqqqq");
+    parser.parseFromString("truqqqqqq");
   }
 
   @Test
@@ -99,7 +100,7 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON BOOLEAN ERROR(L1:C2): expected input \"false\"\nfake\n  ^");
-    parser.parseFromSource("fake");
+    parser.parseFromString("fake");
   }
 
   @Test
@@ -108,79 +109,79 @@ public class ParseExceptionsTest
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON NULL ERROR(L1:C2): expected input \"null\"\nnunya\n  ^");
-    parser.parseFromSource("nunya");
+    parser.parseFromString("nunya");
   }
-  
+
   @Test
   public void stringTest1()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON STRING ERROR(L1:C6): no closing \" before end of input\n\"string\n      ^");
-    parser.parseFromSource("\"string");
+    parser.parseFromString("\"string");
   }
-  
+
   @Test
   public void stringTest2()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON STRING ERROR(L1:C8): invalid escape character \\q\n\"string \\q\"\n        ^");
-    parser.parseFromSource("\"string \\q\"");
+    parser.parseFromString("\"string \\q\"");
   }
-  
+
   @Test
   public void stringTest3()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON STRING ERROR(L1:C1): invalid escape character \\x\n\"\\x\"\n ^");
-    parser.parseFromSource("\"\\x\"");
+    parser.parseFromString("\"\\x\"");
   }
-  
+
   @Test
   public void stringTest4()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON STRING ERROR(L1:C8): unfinished escape character, no closing \" before end of input\n\"string \\\n        ^");
-    parser.parseFromSource("\"string \\");
+    parser.parseFromString("\"string \\");
   }
-  
+
   @Test
   public void arrayTest1()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON ARRAY ERROR(L1:C19): no closing ] before end of input\n[ true, null, false \n                   ^");
-    parser.parseFromSource("[ true, null, false ");
+    parser.parseFromString("[ true, null, false ");
   }
-  
+
   @Test
   public void objectTest1()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON Object ERROR(L1:C12): no closing } before end of input\n{ \"one\":true \n            ^");
-    parser.parseFromSource("{ \"one\":true ");
+    parser.parseFromString("{ \"one\":true ");
   }
-  
+
   @Test
   public void objectTest2()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON OBJECT ERROR(L1:C1): invalid character 'f', expected start of key string\n{ false:true }\n ^");
-    parser.parseFromSource("{ false:true }");
+    parser.parseFromString("{ false:true }");
   }
-  
+
   @Test
   public void objectTest3()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON OBJECT ERROR(L1:C8): invalid character '@', expected value for key:value pair\n{ \"test\"@true }\n        ^");
-    parser.parseFromSource("{ \"test\"@true }");
+    parser.parseFromString("{ \"test\"@true }");
   }
-  
+
 }
