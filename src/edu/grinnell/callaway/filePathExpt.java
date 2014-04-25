7e58dc13e9540efc849a4802ee70f1bf6266957c
mulhalle <mulhalle@grinnell.edu>
2014-04-25 11:14:23 -0500

    add filepath experiment

---
+++ src/edu/grinnell/callaway/filePathExpt.java
@@ -0,0 +1,11 @@
+package edu.grinnell.callaway;
+
+public class filePathExpt
+{
+  // http://stackoverflow.com/questions/4871051/getting-the-current-working-directory-in-java
+  public static void main(String[] args)
+  {
+    System.out.println("Working Directory = " + System.getProperty("user.dir"));
+  }
+
+}
