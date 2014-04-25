51493d579bf3a6629f576639c4eaf0963341a833
mulhalle <mulhalle@grinnell.edu>
2014-04-25 11:19:22 -0500

    add tests for true, false, null, and string

--- src/edu/grinnell/callaway/ParseExceptionsTest.java
+++ src/edu/grinnell/callaway/ParseExceptionsTest.java
@@ -13,16 +13,52 @@ public class ParseExceptionsTest
   public ExpectedException thrown = ExpectedException.none();
 
   @Test
-  public void parserTest()
+  public void noJasonTest1()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON ERROR: no json values found in input");
     parser.parseFromSource("");
   }
-  
+
   @Test
-  public void numberTest2()
+  public void noJasonTest2()
+    throws Exception
+  {
+    thrown.expect(Exception.class);
+    thrown.expectMessage("\nJSON ERROR: no json values found in input");
+    parser.parseFromSource(" \t \n \r \b \f ");
+  }
+
+  @Test
+  public void invalidCharTest1()
+    throws Exception
+  {
+    thrown.expect(Exception.class);
+    thrown.expectMessage("\nJSON ERROR(L1:C0): Invalid input character a\na\n^");
+    parser.parseFromSource("a");
+  }
+
+  @Test
+  public void invalidCharTest2()
+    throws Exception
+  {
+    thrown.expect(Exception.class);
+    thrown.expectMessage("\nJSON ERROR(L1:C0): Invalid input character @\n@\n^");
+    parser.parseFromSource("@");
+  }
+
+  @Test
+  public void invalidCharTest3()
+    throws Exception
+  {
+    thrown.expect(Exception.class);
+    thrown.expectMessage("\nJSON ERROR(L1:C0): Invalid input character }\n}\n^");
+    parser.parseFromSource("}");
+  }
+
+  @Test
+  public void numberTest1()
     throws Exception
   {
     thrown.expect(Exception.class);
@@ -30,13 +66,48 @@ public class ParseExceptionsTest
     parser.parseFromSource("15-");
   }
 
-
   @Test
-  public void runNumberTest3()
+  public void numberTest2()
     throws Exception
   {
     thrown.expect(Exception.class);
     thrown.expectMessage("\nJSON NUMBER ERROR(L1:C0): invalid number 13+\n13+\n^");
     parser.parseFromSource("13+");
+  }
+
+  @Test
+  public void booleanTrueTest()
+    throws Exception
+  {
+    thrown.expect(Exception.class);
+    thrown.expectMessage("\nJSON BOOLEAN ERROR(L1:C4): expected input \"true\"\ntruqqqqqq\n    ^");
+    parser.parseFromSource("truqqqqqq");
+  }
+
+  @Test
+  public void booleanFalseTest()
+    throws Exception
+  {
+    thrown.expect(Exception.class);
+    thrown.expectMessage("\nJSON BOOLEAN ERROR(L1:C3): expected input \"false\"\nfake\n   ^");
+    parser.parseFromSource("fake");
+  }
+
+  @Test
+  public void nullTest()
+    throws Exception
+  {
+    thrown.expect(Exception.class);
+    thrown.expectMessage("\nJSON NULL ERROR(L1:C3): expected input \"null\"\nnunya\n   ^");
+    parser.parseFromSource("nunya");
+  }
+  
+  @Test
+  public void stringTest1()
+    throws Exception
+  {
+    thrown.expect(Exception.class);
+    thrown.expectMessage("\nJSON STRING ERROR(L1:C8): no closing \" before end of input\n\"string\\\n        ^");
+    parser.parseFromSource("\"string\\");
   }
 }
