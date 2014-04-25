e68fdba5cb26b60c4340ebd0b60d1dc4a95410ca
emcallaway <callaway@grinnell.edu>
2014-04-24 22:27:57 -0500

    added some tests for exceptions

--- src/edu/grinnell/callaway/JSONTester.java
+++ src/edu/grinnell/callaway/JSONTester.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 public class JSONTester
 {
   JSONParser parser = new JSONParser();
-  ToString str = new ToString();
 
   /**
    * Test the individual values with toStr
@@ -22,16 +21,17 @@ public class JSONTester
   public void toStrTestVals()
     throws Exception
   {
-    assertEquals("1. true", "true", str.toStr(true));
-    assertEquals("2. false", "false", str.toStr(false));
-    assertEquals("3. num", "2.34", str.toStr(2.34));
-    assertEquals("4. enum", "2.3E35", str.toStr(23e34));
-    assertEquals("5. null", "null", str.toStr(null));
-    assertEquals("6. string", "\"a string\"", str.toStr("a string"));
+    assertEquals("1. true", "true", parser.toStr(true));
+    assertEquals("2. false", "false", parser.toStr(false));
+    assertEquals("3. num", "2.34", parser.toStr(2.34));
+    assertEquals("4. enum", "2.3E35", parser.toStr(23e34));
+    assertEquals("5. null", "null", parser.toStr(null));
+    assertEquals("6. string", "\"a string\"", parser.toStr("a string"));
   } // toStrTestVals
 
   /**
    * Test Objects and Vectors with toStr
+   * 
    * @throws Exception
    */
   @Test
@@ -44,25 +44,26 @@ public class JSONTester
     vector.add(1);
     vector.add(2);
 
-    assertEquals("1. vector alone", "[1,2]", str.toStr(vector));
+    assertEquals("1. vector alone", "[1,2]", parser.toStr(vector));
 
     Vector<Serializable> vector2 = new Vector<Serializable>(3);
     vector2.add(vector);
     vector2.add(2);
 
-    assertEquals("2. vector in vector", "[[1,2],2]", str.toStr(vector2));
+    assertEquals("2. vector in vector", "[[1,2],2]", parser.toStr(vector2));
 
     HashMap<String, Object> hmap = new HashMap<String, Object>(3);
 
     hmap.put("x", "it");
     hmap.put("y", 2.7e-4);
 
-    assertEquals("3. hashMap", "{\"y\":2.7E-4,\"x\":\"it\"}", str.toStr(hmap));
+    assertEquals("3. hashMap", "{\"y\":2.7E-4,\"x\":\"it\"}",
+                 parser.toStr(hmap));
 
     vector.add(hmap);
 
     assertEquals("4. hashMap in vector", "[1,2,{\"y\":2.7E-4,\"x\":\"it\"}]",
-                 str.toStr(vector));
+                 parser.toStr(vector));
 
     hash.put("a", true);
     hash.put("b", 2.7e-4);
@@ -78,9 +79,13 @@ public class JSONTester
                      + "\"e\":null,"
                      + "\"j\":[[1,2,{\"y\":2.7E-4,\"x\":\"it\"}],2],"
                      + "\"b\":2.7E-4," + "\"c\":\"The\"," + "\"a\":true}",
-                 str.toStr(hash));
+                 parser.toStr(hash));
   } // toStrTestObVec
 
+  /**
+   * Tests parse() from JSONParser for correct output.
+   * @throws Exception
+   */
   @Test
   public void parseTest()
     throws Exception
@@ -88,23 +93,72 @@ public class JSONTester
     assertEquals("1. JSON string",
                  "{\"Pro\":{\"FName\":\"Sam\",\"LName\":\"Rebelsky\"},"
                      + "\"Number\":207,\"Department\":\"CSC\"}",
-                 str.toStr(parser.parse("{\"Department\":\"CSC\","
-                                        + " \"Number\":207, "
-                                        + "\"Pro\":{\"LName\":\"Rebelsky\","
-                                        + "\"FName\":\"Sam\"}}")));
-    assertEquals("2. array", "[1,2,3]", str.toStr(parser.parse("[1,2,3]")));
-    assertEquals("3. number", "34", str.toStr(parser.parse("34")));
-    assertEquals("4. decimal", "34.6", str.toStr(parser.parse("34.6")));
-    assertEquals("5. enum", "3E+32", str.toStr(parser.parse("3e32")));
-    assertEquals("6. null", "null", str.toStr(parser.parse("null")));
-    assertEquals("7. true", "true", str.toStr(parser.parse("true")));
-    assertEquals("8. false", "false", str.toStr(parser.parse("false")));
-    assertEquals("9. negative", "-3.4", str.toStr(parser.parse("-3.4")));
+                 parser.toStr(parser.parse("{\"Department\":\"CSC\","
+                                           + " \"Number\":207, "
+                                           + "\"Pro\":{\"LName\":\"Rebelsky\","
+                                           + "\"FName\":\"Sam\"}}")));
+    assertEquals("2. array", "[1,2,3]", parser.toStr(parser.parse("[1,2,3]")));
+    assertEquals("3. number", "34", parser.toStr(parser.parse("34")));
+    assertEquals("4. decimal", "34.6", parser.toStr(parser.parse("34.6")));
+    assertEquals("5. enum", "3E+32", parser.toStr(parser.parse("3e32")));
+    assertEquals("6. null", "null", parser.toStr(parser.parse("null")));
+    assertEquals("7. true", "true", parser.toStr(parser.parse("true")));
+    assertEquals("8. false", "false", parser.toStr(parser.parse("false")));
+    assertEquals("9. negative", "-3.4", parser.toStr(parser.parse("-3.4")));
+    assertEquals("10. string", "\"string\"",
+                 parser.toStr(parser.parse("\"string\"")));
 
-    assertEquals("10. things within things",
+    assertEquals("11. things within things",
                  "{\"f\":{\"y\":2.9E+57,\"x\":\"it\"},"
                      + "\"e\":null,\"c\":\"The\",\"j\":[1,2]}",
-                 str.toStr(parser.parse("{\"f\":{\"y\":29e56,\"x\":\"it\"},"
-                                        + "\"e\":null,\"j\":[1,2],\"c\":\"The\"}")));
+                 parser.toStr(parser.parse("{\"f\":{\"y\":29e56,\"x\":\"it\"},"
+                                           + "\"e\":null,\"j\":[1,2],\"c\":\"The\"}")));
   } // parseTest()
+
+  /**
+   * Helper procedure to test if an exception is properly thrown.
+   * @param str
+   * @param command
+   */
+  public void checkException(String str, String command)
+  {
+    try
+      {
+        switch (command)
+          {
+            case "parse":
+              parser.parse(str);
+              break;
+          /*  case "num":
+              parser.parseNum(str);
+              break;
+            case "null":
+              parser.parseNull(str);
+              break;*/
+          }
+        fail("Incorrect error handling.");
+      } // try
+    catch (Exception e)
+      {
+      } // catch
+  } // checkException(string, string)
+
+  /**
+   * Tests to make sure exceptions are thrown when input is incorrect.
+   * @throws Exception
+   */
+  @Test
+  public void errorMessageTest()
+    throws Exception
+  {
+    checkException("r", "parse");
+    checkException("[", "parse");
+    checkException("}", "parse");
+    checkException("", "parse");
+    //checkException("12s3", "parse");  // ?? This should be an exception, right?
+    checkException("{123}", "parse");
+    // checkException("[123,]", "parse"); // ?? Exception?
+    checkException("e23fds", "parse");
+    parser.parse("\"e23fds\"");
+  } // errorMessageTest()
 } // class JSONTester
