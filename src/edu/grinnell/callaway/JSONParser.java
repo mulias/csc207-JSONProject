f51265c6455a58bf20e7bc52c646815eb184248c
mulhalle <mulhalle@grinnell.edu>
2014-04-25 18:07:34 -0500

    add comments and javadoc

--- src/edu/grinnell/callaway/JSONParser.java
+++ src/edu/grinnell/callaway/JSONParser.java
@@ -27,67 +27,100 @@ public class JSONParser
   /*
    * CITATION:
    * http://stackoverflow.com/questions/15842239/how-to-cast-a-string-to-an-url-in-java http://www.mkyong.com/java/how-to-get-url-content-in-java/
-   * http://www.mkyong.com/java/how-to-read-file-from-java-bufferedreader-example/
-   * http://www.tutorialspoint.com/design_pattern/mvc_pattern.htm
    */
-  public Object parseFromSource(String jsonSource)
+  /**
+   * Read and parse json located at a web address and return a java object representing the json code.
+   *  
+   * @param address - the url pointing to the json file
+   * @return Object - the object described by json code
+   * @throws Exception
+   */
+  public Object parseFromHTTP(String address)
     throws Exception
   {
-    // return normal empty string error, do not check first char
-    if (jsonSource.isEmpty())
-      {
-        return parse(new StringReader(jsonSource));
-      }
-    // URL start with an "h", as in "http(s)..."
-    // file locations start with a "/", "/home/..."
-    // otherwise assume source is a json string
+    // make a new stream reader to read the file
+    // pass the reader to the parse(Reader) method
     try
       {
-        char firstChar = jsonSource.charAt(0);
-        if (firstChar == 'h')
-          {
-            // get URL, open connection
-            URL url = new URL(jsonSource);
-            URLConnection connect = url.openConnection();
-            return parse(new InputStreamReader(connect.getInputStream()));
-          }
-        // Support for both linux (/) and Windows style paths (starting with
-        // either C: or \)
-        else if (firstChar == '/' || firstChar == 'C' || firstChar == '\\')
-          {
-            // a local file
-            return parse(new FileReader(jsonSource));
-          }
-        else
-          {
-            // a string
-            return parse(new StringReader(jsonSource));
-          }
-      }
+        // get URL, open connection
+        URL url = new URL(address);
+        URLConnection connect = url.openConnection();
+        return parse(new InputStreamReader(connect.getInputStream()));
+      } // try
     catch (MalformedURLException e)
       {
         throw new MalformedURLException("MalformedURLException: " + e);
-      }
+      } // catch bad url
+  } // parseFromHTTP(String)
+
+  /*
+   * CITATION:
+   * http://www.mkyong.com/java/how-to-read-file-from-java-bufferedreader-example/
+   */
+  /**
+   * Read and parse json from a file and return a java object representing the json code.
+   *
+   * @param path - file path to the file to read 
+   * @return Object - the object described by json code
+   * @throws Exception
+   */
+  public Object parseFromFile(String path)
+    throws Exception
+  {
+    // make a new file reader to read the file
+    // pass the reader to the parse(Reader) method
+    try
+      {
+        return parse(new FileReader(path));
+      } // try
     catch (FileNotFoundException e)
       {
         throw new FileNotFoundException("");
-      }
-  }// parseFromSource(String)
+      } // catch bad file name
+  } // parseFromFile(String)
 
+  /**
+   * Read and parse a string of json and return a java object representing the json code.
+   * 
+   * @param str - string of json code
+   * @return Object  - the object described by json code
+   * @throws Exception
+   */
+  public Object parseFromString(String str)
+    throws Exception
+  {
+    // make a new string reader to pass to parse(Reader)
+    // take any string, parse function will return errors
+    return parse(new StringReader(str));
+  } // parseFromString(String)
+
+  /**
+   * Read and parse json from any java character stream reader, 
+   * including FileReader, InputStreamReader, StringReader, 
+   * BufferedReader, and CharArrayReader. Return a java object 
+   * representing the json code.
+   * @param in - any Reader
+   * @return Object - the object described by json code
+   * @throws Exception
+   */
   public Object parse(Reader in)
     throws Exception
   {
+    // pass the reader into an IndexedBufferedReader
+    // get the json object from the input
+    // close the buffer, which also closes the stream
     IndexedBufferedReader buffer = new IndexedBufferedReader(in);
     Object json = parse(buffer);
     buffer.close();
     return json;
-  }// Object parse(String str)
+  } // Object parse(String str)
 
   /**
-   * parse JSON buffer to java Object
+   * Read the buffer and return each json value found as an object
    * 
-   * @param buffer
-   * @return Object
+   * @param buffer - buffered reader to track location in input, 
+   *  as well as improve error messages
+   * @return Object - the object described by json code
    * @pre buffer is formated JSON code
    * @post the JSON string has been translated into a java object
    * @throws Exception
@@ -95,15 +128,14 @@ public class JSONParser
   public Object parse(IndexedBufferedReader buffer)
     throws Exception
   {
-    // mark each space in buffer before advancing one
-    // advance through each char in buffer
-    // ignore whitespace
-    // if char signals the opener of a Object, such as {, [, or "
-    // // parse that object starting at the next char
-    // if char is the first char in a Object, such as t in true or 1 in 125
-    // // use the mark to back up one char and then parse the object
-    // if the end of the buffer is reached before an object is found
-    // // throw exception
+    // for each character in the buffer
+    // ignore whitespace, identify json objects, 
+    // arrays, strings, booleans, null, and numbers
+    // and parse each value found
+    // if a char can not be identified as the start of
+    // a json value, throw exception
+    // if the end of the buffer is reached before finding
+    // a json value, throw an exception
     int c;
     while ((c = buffer.peek()) != -1)
       {
@@ -164,22 +196,24 @@ public class JSONParser
   } // parse(IndexedBufferedReader)
 
   /**
-   * Parse JSON string/bufferedReader into a java BigDecimal
+   * Read the buffer for json number values
+   * return json number as a java BigDecimal
    * 
    * @pre buffer is an element of REAL NUMBERS
    * @post string/bufferedreader is parsed to java BigDecimal
-   * @param buffer
-   * @return
+   * @param buffer - buffered reader of json code
+   * @return BigDecimal - json number
    * @throws Exception
    */
   public BigDecimal parseNum(IndexedBufferedReader buffer)
     throws Exception
   {
     // use a StringBuilder to stick nums together
-    // when we reach a non-number char
-    // // stop, use str.reset() to backup one
-    // try to make a BigDecimal
-    // if the syntax is bad it will throw an exception
+    // move through the buffer until a non-number 
+    // value is found
+    // try to make a BigDecimal from the string
+    // if the number is formatted right it will succeed
+    // otherwise throw exception
     StringBuilder builder = new StringBuilder();
     boolean num_end = false;
     while (!num_end)
@@ -221,13 +255,14 @@ public class JSONParser
                                                            + number, buffer,
                                       -number.length() + 1));
       } // catch
-  } // parseNum(BufferedReader)
+  } // parseNum(IndexedBufferedReader)
 
   /**
-   * Parse JSON string to java null
+   * Read the buffer for json null value
+   * return json null as a java null
    * 
-   * @param buffer
-   * @pre buffer must be "null"
+   * @param buffer - buffered reader of json code
+   * @pre first three buffer chars must be "null"
    * @return
    * @throws Exception
    */
@@ -243,16 +278,17 @@ public class JSONParser
           {
             throw new Exception(jsonError("JSON NULL ERROR",
                                           "expected input \"null\"", buffer));
-          }
-      }
+          } // if
+      } // for
     return null;
   } // Object parseNull(IndexedBufferedReader)
 
   /**
-   * Parse JSON to java boolean (true)
+   * Read the buffer for json true value
+   * return json true as a java boolean
    * 
-   * @param buffer
-   * @pre buffer must be "true"
+   * @param buffer - buffered reader of json code
+   * @pre first four buffer chars must be "true"
    * @throws Exception
    */
   public Boolean parseTrue(IndexedBufferedReader buffer)
@@ -273,9 +309,10 @@ public class JSONParser
   }// parseTrue(IndexedBufferedReader)
 
   /**
-   * Parse JSON to java boolean (false)
+   * Read the buffer for json false value
+   * return json true as a java boolean
    * 
-   * @param buffer
+   * @param buffer - buffered reader of json code
    * @pre buffer must be "false"
    * @throws Exception
    */
@@ -297,34 +334,28 @@ public class JSONParser
   } // parseFalse(IndexedBufferedReader)
 
   /**
-   * Parse JSON string
+   * Read the buffer for a json string value
+   * return json string as a java String
    * 
-   * @pre buffer is 'chain'(string) of characters and must start with either '"'
-   *      or '\'
-   * @param buffer
+   * @param buffer - buffered reader of json code
    * @return Java string
    * @throws Exception
    */
   public String parseString(IndexedBufferedReader buffer)
     throws Exception
   {
-    // save each char to a StringBuilder
-    // if c = \
-    // // check if next is " accepted non-whitespace escape char, add it
-    // // else throw exception
-    // if c = "
-    // // done
-    // if end of buffer
-    // throw no closing " error
-    // if c is any other char
-    // // add it
+    // save each char to a stringbuilder
+    // string must start with "
+    // if an escape is found, parse with parseEscapeChar
+    // if the end of the buffer is reached, throw exception
+    // otherwise add each char until closing quote is found
     StringBuilder builder = new StringBuilder();
     int c;
     if (buffer.read() != '"')
       {
         throw new Exception(jsonError("JSON STRING ERROR",
                                       "string must start with \"", buffer));
-      }
+      } // if
     while ((c = buffer.peek()) != '"')
       {
         switch (c)
@@ -344,64 +375,91 @@ public class JSONParser
               break;
           }// switch(C)
       }// while
+    // read closing quote
     buffer.read();
     return builder.toString();
   }// String parseString(IndexedBufferedReader)
 
+  /**
+   * Read the buffer for a json valid escape character
+   * return escape char as a java char
+   * 
+   * @param buffer - buffered reader of json code
+   * @return
+   * @throws Exception
+   */
   public char parseEscapeChar(IndexedBufferedReader buffer)
     throws Exception
   {
+    // escape char must start with \
+    // if it's " or \ or /, return the char
+    // if it's 'u', make a new unicode hex-based char
+    // if the end of the buffer is reached before resolving, throw exception
+    // if the char is not a valid escape char, throw exception
     int c = buffer.read();
     if (c != '\\')
       {
         throw new Exception(jsonError("JSON STRING ERROR",
                                       "escape characters must start with \\",
                                       buffer));
-      }
+      } // if
     else
       {
         c = buffer.read();
         if (c == '"' || c == '\\' || c == '/')
           {
+            // basic escape chars, return value
             return (char) c;
-          }
+          } // if
         else if (c == 'u')
           {
+            //get the four hex numbers
             String unicode = "";
             for (int i = 0; i < 4; i++)
               {
                 char num = (char) buffer.read();
                 if (Character.isDigit(num) || Character.isAlphabetic(num))
                   {
+                    // valid unicode
                     unicode += num;
-                  }
+                  } // if
                 else
                   {
-                    throw new Exception("");
-                  }
-              }
+                    // invalid
+                    throw new Exception(
+                                        jsonError("JSON STRING ERROR",
+                                                  "invalid unicode char \\u"
+                                                      + unicode
+                                                      + num
+                                                      + ", "
+                                                      + num
+                                                      + " is not a valid hex-number input",
+                                                  buffer));
+                  } // else
+              } // for
             return (char) Integer.parseInt(unicode, 16);
-          }
+          } // else if u
         else if (c == -1)
           {
             throw new Exception(
                                 jsonError("JSON STRING ERROR",
                                           "unfinished escape character, no closing \" before end of input",
                                           buffer));
-          }
+          } // else if -1
         else
           {
             throw new Exception(jsonError("JSON STRING ERROR",
                                           "invalid escape character \\"
                                               + (char) c, buffer, -1));
-          }
+          } // else
       } // else
-  }
+  } // parseEscapeChar(IndexedBufferedReader)
 
   /**
-   * Parse Jason Array
+   * Read the buffer for a json array of json values
+   * return json array as java Vector of Objects
    * 
-   * @param buffer
+   * @param buffer - buffered reader of json code
    * @pre buffer must start with '[' '[' can not be followed by ','
    * @return Vector
    * @throws Exception
@@ -415,17 +473,15 @@ public class JSONParser
     // // search for any char, send it to parse
     // // save value to vector
     // if we have found a value
-    // // search for ','
-    // // throw exception if not found
     // // search for new value
-    // if end of buffer
+    // if unexpected end of buffer
     // // throw exception
     Vector<Object> vec = new Vector<Object>();
     int c;
     if (buffer.read() != '[')
       {
         throw new Exception("");
-      }
+      } // if
     while ((c = buffer.peek()) != ']')
       {
         switch (c)
@@ -452,14 +508,16 @@ public class JSONParser
               break;
           }// switch (c)
       }// while
+    // read closing ]
     buffer.read();
     return vec;
   }// parseArray(IndexedBufferedReader)
 
   /**
-   * Parse Jason Object
+   * Read the buffer for a json object of json key:value pairs
+   * return json object as java HashMap
    * 
-   * @param buffer
+   * @param buffer - buffered reader of json code
    * @pre buffer must start with '{' '{' must be followed by type 'string' i.e a
    *      '"' and a ':'
    * @return HashMap
@@ -479,7 +537,7 @@ public class JSONParser
     // if we have a key and a value
     // // save to hash
     // // look for new pair
-    // if end of buffer
+    // if unexpected end of buffer
     // // throw exception
     HashMap<String, Object> hash = new HashMap<String, Object>();
     boolean key_found = false;
@@ -491,7 +549,7 @@ public class JSONParser
     if (buffer.read() != '{')
       {
         throw new Exception("");
-      }
+      } // if
     while ((c = buffer.peek()) != '}')
       {
         switch (c)
@@ -518,7 +576,7 @@ public class JSONParser
                     {
                       key = (String) parse(buffer);
                       key_found = true;
-                    }
+                    } // if
                   else
                     {
                       throw new Exception(
@@ -527,8 +585,8 @@ public class JSONParser
                                                         + (char) c
                                                         + "', expected start of key string",
                                                     buffer));
-                    }
-                }
+                    } // else
+                } // if
               // else if we don't have a value, the next char should be ':'
               else if (!value_found)
                 {
@@ -536,7 +594,7 @@ public class JSONParser
                     {
                       value = parse(buffer);
                       value_found = true;
-                    }
+                    } // if
                   else
                     {
                       throw new Exception(
@@ -545,28 +603,52 @@ public class JSONParser
                                                         + (char) c
                                                         + "', expected value for key:value pair",
                                                     buffer));
-                    }
-                }
+                    } // else
+                } // else if
               break;
-          }
+          } // switch(c)
         if (key_found && value_found)
           {
             hash.put(key, value);
             key_found = false;
             value_found = false;
-          }
+          } // if
       } // while
+    // read closing }
     buffer.read();
     return hash;
-  }// parseObject(IndexedBufferedReader)
+  } // parseObject(IndexedBufferedReader)
 
+  /**
+   * Generate a json error message, specifying what kind of json value had 
+   * an error, what the error was, and where in the json code the error occurred
+   * 
+   * @param header - kind of error (string, boolean, number, et)
+   * @param body - error specifics
+   * @param buffer - the IndexedBufferedReader of json code
+   * @return
+   * @throws IOException
+   */
   public String jsonError(String header, String body,
                           IndexedBufferedReader buffer)
     throws IOException
   {
+    // pass to jsonError method with offset of 0
     return jsonError(header, body, buffer, 0);
   }
 
+  /**
+   * Generate a json error message, specifying what kind of json value had 
+   * an error, what the error was, and where in the json code the error occurred
+   * 
+   * @param header - kind of error (string, boolean, number, et)
+   * @param body - error specifics
+   * @param buffer - the IndexedBufferedReader of json code
+   * @param offset - number of spaces to move the error pointer over, 
+   * to point at the beginning of an error
+   * @return
+   * @throws IOException
+   */
   public String jsonError(String header, String body,
                           IndexedBufferedReader buffer, int offset)
     throws IOException
